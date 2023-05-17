05b3b27d467c4e10a03e539b8977a756b466f14c
orzees <130258986+orzees@users.noreply.github.com>
2023-05-17 02:39:47 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 
 # 设置 nginx 伪装站
 rm -rf /usr/share/nginx/*
-wget https://github.com/numia090/backforyou/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+wget https://github.com/Jimyarkw/backFo/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
 unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
